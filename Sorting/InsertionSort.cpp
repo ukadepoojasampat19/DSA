f55bc3d2628dc5eb64ca5d6b9a8d8e7b566b529e
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2023-08-13 10:54:44 +0530

    InsertionSort.cpp

---
+++ Sorting/InsertionSort.cpp
@@ -0,0 +1,55 @@
+/*Insert the element from the unsorted array to its correct position in the sorted array.*/
+/*
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int temp;
+    cout<<"Enter the size of the array; ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=1;i<n;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            if(arr[i] < arr[j])
+            {
+                temp=arr[i];
+                 arr[i]=arr[j];
+                 arr[j]=temp;
+              
+
+             }
+        }
+        
+        cout<<"\n";
+        for(int i=0;i<n;i++)
+        {
+            cout<<arr[i]<<" ";
+        }
+        cout<<"\n";
+
+        
+    }
+    cout<<"Sorted array:\n ";
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
+*/
+//fastest three horses.
+/*You have 25 horses (find which horse run fast)
+at max 5 can race together.
+Minimum races for fastest 3 
+ANS=> */
+
