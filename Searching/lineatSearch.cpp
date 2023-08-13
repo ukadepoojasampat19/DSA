d87012465465203ef607b848f018aa586e4b3668
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2023-08-13 10:52:39 +0530

    lineatSearch.cpp

---
+++ Searching/lineatSearch.cpp
@@ -0,0 +1,44 @@
+
+// LINEAR SEARCH:to check with the every element in the array lineraly(one by one.)
+/*#include<iostream>
+#include<climits>
+
+using namespace std;
+int main()
+{
+int n,element,i;
+
+cout<<"Enter the size of the array: ";
+
+cin>>n;
+int arr[n];
+bool status;
+status=false;
+for(int i=0;i<n;i++)
+{
+    cin>>arr[i];
+}
+cout<<"Enter the elemnt you want to search: ";
+cin>>element;
+for(i=0;i<n;i++)
+{
+    if(element == arr[i])
+    {
+        cout<<"index"<<""<<i<<"Match";
+        status=true;
+        
+        break;
+    }
+    else
+    {
+        continue;
+    }
+}
+if(status == false)
+{
+    cout<<"element does not matched with any of the element in the array.";
+}
+
+return 0;
+}
+*/
